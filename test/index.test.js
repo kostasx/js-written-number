667adf61a6ef592aa87dea332a47661a78292107
Kostas Minaidis <kostas@creativecollective.gr>
2015-04-16 20:40:29 +0300

    Multilingual Number conversion. Working on Greek numbers.

--- test/index.test.js
+++ test/index.test.js
@@ -34,5 +34,44 @@ describe('written-number', function() {
       writtenNumber(4323000).should.equal('four million three hundred twenty-three thousand');
       writtenNumber(4323055).should.equal('four million three hundred twenty-three thousand and fifty-five');
     });
+
+  });
+});
+
+/*** GREEK LANGUAGE TEST ***/
+
+describe('written-number in Greek', function() {
+  describe('writtenNumber(n, { lang: "el" })', function() {
+    it('gets exposed', function() {
+      should.exist(writtenNumber);
+      writtenNumber.should.be.instanceof(Function);
+    });
+
+    it('correctly converts numbers < 10', function() {
+      writtenNumber(3, { lang: "el" }).should.equal('τρία');
+      writtenNumber(8, { lang: "el" }).should.equal('οκτώ');
+    });
+
+    it('correctly converts numbers < 20', function() {
+      writtenNumber(13, { lang: "el" }).should.equal('δεκατρία');
+      writtenNumber(19, { lang: "el" }).should.equal('δεκαεννιά');
+    });
+
+    it('correctly converts numbers < 100', function() {
+      writtenNumber(20, { lang: "el" }).should.equal('είκοσι');
+      writtenNumber(25, { lang: "el" }).should.equal('είκοσι-πέντε');
+      writtenNumber(88, { lang: "el" }).should.equal('ογδόντα-οκτώ');
+      writtenNumber(73, { lang: "el" }).should.equal('εβδομήντα-τρία');
+    });
+
+    it('correctly converts numbers < 1000', function() {
+      writtenNumber(200, { lang: "el" }).should.equal('διακόσια');
+      writtenNumber(1234, { lang: "el" }).should.equal('χίλια διακόσια τριάντα-τέσσερα');
+      writtenNumber(4323, { lang: "el" }).should.equal('τέσσερις χιλιάδες τριακόσια είκοσι-τρία');
+      writtenNumber(242, { lang: "el" }).should.equal('διακόσια σαράντα δύο');
+      writtenNumber(4323000, { lang: "el" }).should.equal('τέσσερα εκατομμύρια τριακόσιες είκοσι τρεις χιλιάδες');
+      writtenNumber(4323055, { lang: "el" }).should.equal('τέσσερα εκατομμύρια τριακόσιες είκοσι τρεις χιλιάδες και πενήντα πέντε');
+    });
+
   });
 });
